2f0d4cd2ad3e961a54b1053955abd06e54c04a38
YoonJunHyeok <jhyoon1607@gmail.com>
2024-09-13 16:40:47 +0900

    feat: s3 outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,6 +11,17 @@ output "ec2_public_ip" {
 
 # S3 Outputs
 output "s3_bucket_name" {
-  value = aws_s3_bucket.my_bucket.bucket
+  value = aws_s3_bucket.main.bucket
   description = "The name of the S3 bucket"
+}
+
+output "s3_user_access_key" {
+  value       = aws_iam_access_key.s3_user_access_key.id
+  description = "Access key for the IAM user"
+}
+
+output "s3_user_secret_key" {
+  value       = aws_iam_access_key.s3_user_access_key.secret
+  description = "Secret access key for the IAM user"
+  sensitive   = true
 }
