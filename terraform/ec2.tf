a852e7ed43fc1d3b0a1d105cd32b07a95179cf1e
YoonJunHyeok <jhyoon1607@gmail.com>
2025-03-24 21:23:45 +0900

    format

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "main" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public.id
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_web.id]
 
   tags = {
@@ -9,17 +9,17 @@ resource "aws_instance" "main" {
   }
 
   user_data = <<-EOF
-              #!/bin/bash
-              # Install Docker
-              sudo yum update -y
-              sudo yum install docker -y
-              sudo service docker start
-              sudo systemctl enable docker
-              sudo usermod -aG docker ec2-user
+    #!/bin/bash
+    # Install Docker
+    sudo yum update -y
+    sudo yum install docker -y
+    sudo service docker start
+    sudo systemctl enable docker
+    sudo usermod -aG docker ec2-user
 
-              # Install Nginx
-              sudo yum install nginx -y
-              sudo systemctl start nginx
-              sudo systemctl enable nginx
-              EOF
+    # Install Nginx
+    sudo yum install nginx -y
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+  EOF
 }
