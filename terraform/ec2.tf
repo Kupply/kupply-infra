502fe8943a7eac0dd217cea060aa75518c32c764
YoonJunHyeok <jhyoon1607@gmail.com>
2025-03-25 23:37:17 +0900

    fix: hard code

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,8 +18,7 @@ sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 
 # Install Docker Compose
-DOCKER_COMPOSE_VERSION="v2.34.0"
-sudo curl -L "https://github.com/docker/compose/releases/download/\${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.34.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Install Nginx
