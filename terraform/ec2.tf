f2355929247af4816a53474b122301bece8023af
YoonJunHyeok <jhyoon1607@gmail.com>
2025-03-25 23:24:39 +0900

    feat: install docker compose

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -17,6 +17,11 @@ resource "aws_instance" "main" {
     sudo systemctl enable docker
     sudo usermod -aG docker ec2-user
 
+    # Install Docker Compose
+    DOCKER_COMPOSE_VERSION="v2.34.0"
+    sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
     # Install Nginx
     sudo yum install nginx -y
     sudo systemctl start nginx
